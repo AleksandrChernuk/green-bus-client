62439a0d32a3e0f098443f9c417f8d141c9f11fc
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-06 00:53:57 +0200

    checkoutPage 2

--- src/app/[locales]/(root)/new-order/page.tsx
+++ src/app/[locales]/(root)/new-order/page.tsx
@@ -1,7 +1,12 @@
 import SecondFooter from '@/components/modules/footer/SecondFooter';
 import NewOrderPage from '@/components/pages/NewOrderPage';
+import { cookies } from 'next/headers';
 
 export default async function NewOrder() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get('adult');
+
+  console.log(theme);
   return (
     <>
       <main role='main' className='pb-16 grow bg-grayy dark:bg-background_black_mode'>
