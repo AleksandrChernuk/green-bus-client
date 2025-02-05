28ee4f2d1f23fc488e0e0dca00fff6843059614f
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-05 23:19:41 +0200

    checkoutPage 2

--- src/app/[locales]/(root)/new-order/page.tsx
+++ src/app/[locales]/(root)/new-order/page.tsx
@@ -2,14 +2,19 @@ import SecondFooter from '@/components/modules/footer/SecondFooter';
 import NewOrderPage from '@/components/pages/NewOrderPage';
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
+import { getCookies } from 'cookies-next/server';
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 export default async function NewOrder({ searchParams }: { searchParams: SearchParams }) {
   const slug = await searchParams;
-  const cookieStore = await cookies();
-  console.log('cookieStore', cookieStore.get('adult')?.value);
-  console.log(slug.children);
+ 
+
+
+  const allCookies = await getCookies({ cookies });
+  console.log(allCookies);
+
+
   if (!slug.adult && !slug.children) {
     redirect('/');
   }
