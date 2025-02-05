3e3a38e332e522872c6d8b2efe1921ab9c25fbed
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-05 23:31:36 +0200

    details 2

--- src/i18next.config.ts
+++ src/i18next.config.ts
@@ -1,14 +1,14 @@
 const i18NextConfig = {
-  // debug: false,
+  debug: false,
   i18n: {
     locales: ['en', 'ru', 'uk'],
     defaultLocale: 'uk',
   },
-  // fallbackNS: 'common',
-  // defaultNS: 'common',
-  // ns: ['common'],
-  // load: 'all',
-  // preload: ['en', 'ru', 'uk'],
+  fallbackNS: 'common',
+  defaultNS: 'common',
+  ns: ['common'],
+  load: 'all',
+  preload: ['en', 'ru', 'uk'],
 };
 
 export const getOptions = (lang: string, ns: string | string[]) => {
@@ -16,8 +16,8 @@ export const getOptions = (lang: string, ns: string | string[]) => {
     supportedLangs: i18NextConfig.i18n.locales,
     lang,
     ns,
-    // fallbackNS: i18NextConfig.fallbackNS,
-    // defaultNS: i18NextConfig.defaultNS,
+    fallbackNS: i18NextConfig.fallbackNS,
+    defaultNS: i18NextConfig.defaultNS,
   };
 };
 
