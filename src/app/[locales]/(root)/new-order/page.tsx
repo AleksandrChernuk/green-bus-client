6b11e27884f33d1b669601ab5e9c1533f91c6d98
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-06 00:22:57 +0200

    checkoutPage 2

--- src/app/[locales]/(root)/new-order/page.tsx
+++ src/app/[locales]/(root)/new-order/page.tsx
@@ -1,14 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import SecondFooter from '@/components/modules/footer/SecondFooter';
 import NewOrderPage from '@/components/pages/NewOrderPage';
 import { redirect } from 'next/navigation';
 
- 
-export default async function NewOrder({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const slug = searchParams;
+export default async function NewOrder({ searchParams }: { searchParams: Promise<any> }) {
+  const slug = await searchParams;
 
   if (!slug.adult && !slug.children) {
     redirect('/');
