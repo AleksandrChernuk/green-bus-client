2f35d026dbc429aaead8256d923b687e53b8c963
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-07 01:13:53 +0200

    checkoutPage 2

--- src/components/pages/NewOrderPage/hooks/useCheckoutForm.tsx
+++ src/components/pages/NewOrderPage/hooks/useCheckoutForm.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 /* eslint-disable react-hooks/exhaustive-deps */
 import { CheckoutSchema } from '@/schemas/checkout-form.shema';
 import { FormValues } from '@/types/checkout-from.types';
