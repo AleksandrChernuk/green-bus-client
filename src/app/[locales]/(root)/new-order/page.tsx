126d620d3fb94ee32bc63d6bc47c762ded7ad932
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-07 11:32:38 +0200

    checkoutPage 666

--- src/app/[locales]/(root)/new-order/page.tsx
+++ src/app/[locales]/(root)/new-order/page.tsx
@@ -1,8 +1,7 @@
 import SecondFooter from '@/components/modules/footer/SecondFooter';
 import NewOrderPage from '@/components/pages/NewOrderPage';
 import { cookies } from 'next/headers';
-import { Suspense } from 'react';
-
+ 
 export default async function NewOrder() {
   const cookieStore = await cookies();
   const theme = cookieStore.get('theme') || 'default';
