06a08accfac43bb51b4a8b308e72df596193dcbc
CHerniuk <119289731+AleksandrChernuk@users.noreply.github.com>
2025-02-07 10:52:54 +0200

    checkoutPage 666

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import i18NextConfig from "@/i18next.config";
-import { createInstance, i18n } from "i18next";
-import { initReactI18next } from 'react-i18next/initReactI18next';
-
+import { createInstance, i18n } from 'i18next';
+ 
 import resourcesToBackend from 'i18next-resources-to-backend';
 
 export default async function initTranslations(
